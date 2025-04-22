3f2ad8f987276b8305fa885049e28fd693598aed
Xander Lewis <lewisxb@rose-hulman.edu>
2025-04-22 13:10:35 -0400

    Juggling implemented, curl /angel

---
+++ controllers/AngelController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Controllers;
+
+class AngelController
+{
+  private $requestMethod;
+
+  public function __construct($requestMethod)
+  {
+    $this->requestMethod = $requestMethod;
+    session_start();
+  }
+
+  public function processRequest()
+  {
+    switch ($this->requestMethod) {
+      case 'GET':
+        $response = $this->getUserInfo();
+        break;
+      case 'POST':
+        $response = $this->attemptLogon();
+        break;
+      default:
+        $response = $this->notFoundResponse();
+    }
+    header($response['status_code_header']);
+    if ($response['body']) {
+      echo $response['body'];
+      error_log("body: " . $response['body']);
+    }
+  }
+
+  private function getUserInfo()
+  {
+    $response['status_code_header'] = 'HTTP/1.1 200';
+    return $response;
+  }
+
+  private function attemptLogon()
+  {
+    // $stored_hash = hash("sha256", "34250003024812");
+    $stored_hash = "0e92299296652799688472441889499080435414654298793501210067779366";
+    echo $stored_hash;
+    $user_input = $_POST["password"];
+    $hashed_input = hash("sha256", $user_input);
+    error_log("stored hash: " . $stored_hash . "\ninput: " . $hashed_input);
+
+    // VULNERABILITY: Using '==' to compare can turn both strings to integers, so the hash starting with 0e will cause juggling.
+
+    if ($hashed_input == $stored_hash) {
+      $response['status_code_header'] = 'HTTP/1.1 200 Success';
+      $response['body'] = "Welcome admin, flag is: ctf{hawk_tuah}";
+      error_log("yay");
+      return $response;
+    } else {
+      return $this->notFoundResponse();
+    }
+  }
+
+  private function notFoundResponse()
+  {
+    $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+    $response['body'] = null;
+    return $response;
+  }
+}
--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+
+use Controllers\AngelController;
+
+include("controllers/AngelController.php");
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,PUT,POST,DELETE,GET");
@@ -8,29 +13,22 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 $uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $uri = explode("/", $uri);
 
-$requestMethod = $_SERVER["REQUEST_METHOD"]; 
+$requestMethod = $_SERVER["REQUEST_METHOD"];
 
 $path = null;
 if (isset($uri[2])) {
-    $path = $uri[2];
+  $path = $uri[2];
 }
 
-/*if (isset($uri[1]) && $uri[1] != "index" && $uri[1] != ""){
-    switch ($uri[1]) {
-        case "sleep":
-            $controller = new SleepController($requestMethod, $pdo);
-            $controller->processRequest();
-            break;
-        case "leaderboard":
-        case "users":
-            $controller = new UserController($requestMethod, $path, $pdo);
-            $controller->processRequest();
-            break;
-    }
+if (isset($uri[1]) && $uri[1] != "index" && $uri[1] != "") {
+  switch ($uri[1]) {
+    case "angel":
+      $controller = new AngelController($requestMethod);
+      $controller->processRequest();
+      break;
+    case "leaderboard":
+    case "users":
+  }
 }
-else {
-    header("Location: landing.php");
-}
-*/
 
 header("Location: landing.html");
